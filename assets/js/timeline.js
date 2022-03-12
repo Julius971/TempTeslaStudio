a13aecf704722fc1f68b1d5635440311e39a0062
Dominic Drees <dominic.drees@live.de>
2022-03-12 18:07:18 +0100

    test canvas drawing

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -57,12 +57,65 @@ function download(filename, content) {
     document.body.removeChild(element);
 }
 
+function setUpCanvas() {
+    // get canvas refs
+    const wCtx = document.getElementById("waveform").getContext("2d");
+    const pCtx = document.getElementById("prefix").getContext("2d");
+    const tCtx = document.getElementById("timeline").getContext("2d");
 
-function drawTablePrefixes() {
-    const ctx = document.getElementById("prefix").getContext("2d");
-    ctx.font = '15px serif';
-    ctx.fillStyle = "white";
-    //ctx.fillText('Hello world', 5, 5);
+    // get css variable
+    const tHeight = getComputedStyle(document.documentElement).getPropertyValue('--tHeight').slice(0, -2);
+    const tWidth = getComputedStyle(document.documentElement).getPropertyValue('--tWidth').slice(0, -2);
+    const textColor = getComputedStyle(document.documentElement).getPropertyValue('--secondaryText');
+    const borderColor = getComputedStyle(document.documentElement).getPropertyValue('--borderColor');
+
+    // 150 x width
+    wCtx.canvas.height = 150;
+    wCtx.canvas.width = tWidth;
+
+    // height - 150 x 100
+    pCtx.canvas.height = tHeight - 150;
+    pCtx.canvas.width = 100;
+
+    // height - 150 x width - 100
+    tCtx.canvas.height = tHeight - 150;
+    tCtx.canvas.width = tWidth - 100;
+
+    // style settings
+    wCtx.font = '15px serif';
+    wCtx.fillStyle = textColor;
+    wCtx.strokeStyle = borderColor;
+    wCtx.textAlign = "left";
+
+    pCtx.font = '15px serif';
+    pCtx.fillStyle = textColor;
+    pCtx.strokeStyle = borderColor;
+    pCtx.textAlign = "left";
+
+    tCtx.font = '15px serif';
+    tCtx.fillStyle = textColor;
+    tCtx.strokeStyle = borderColor;
+    tCtx.textAlign = "left";
+
+    return {wCtx, pCtx, tCtx};
 }
 
-drawTablePrefixes()
+function draw() {
+    const {wCtx, pCtx, tCtx} = setUpCanvas();
+
+    // draw prefix cell
+    pCtx.rect(0, 0, 150, 20);
+    pCtx.stroke();
+    pCtx.fillText("Test", 5, 15);
+
+    // draw waveform cell
+    wCtx.rect(101, 130, 20, 20);
+    wCtx.stroke();
+    wCtx.fillText("0", 107, 145);
+
+    // draw timeline cell
+    tCtx.rect(1, 0, 20, 20);
+    tCtx.stroke();
+}
+
+draw()
